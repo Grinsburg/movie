ef2b4ef2763b60dbf63bae6cc974812e09453961
Grinsburg <smlynchik@mail.ru>
2019-12-02 13:23:55 +0300

    add search field

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Search(){
-  return (
-    <div>
-      
+import styles from './Search.module.css';
+
+class Search extends Component{
+  render(){
+    return (
+    <div className={styles.d2}>
+          <input type="text" placeholder="Искать здесь..." />
+          <button type="submit"></button>
     </div>
-  );
+    );
+  }
 }
 
 export default Search;
